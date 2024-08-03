18b559345af0526fcaed8821da865a6c02b2c8a9
Donaghpdmc <donaghpdmc@gmail.com>
2024-08-03 20:19:59 +0000

    Final Stage Of Code Complete with Comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "mbed.h"
 
 /*
+Lab 2: part of this lab we learned how to code a recursive funtion where by the function loops until
+a defined number of loops are obtained. To demonstarte this a Fibonacci squence is simulated, the
+first intial interger and intial subsquent interger of the squence are defined, then addtional function is
+used to otain the next subsquent interger. The process is looped a set number of times, also defined.   
+*/
+
+/*
 The next term is the sum of the previous two terms.
 The first two terms of the Fibonacci sequence are 0 followed by 1.
 */
